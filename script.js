1df27e7cd75064f03e52a627132eeedb6877ea7f
7t4o <hatemlaptop1@gmail.com>
2024-12-21 22:30:01 +0300

    Upload the file

---
+++ script.js
@@ -0,0 +1,17 @@
+function updateClock() {
+    let hour = document.querySelector(".hour")
+    let mint = document.querySelector(".mint")
+    let scnd = document.querySelector(".scnd")
+    
+    const now = new Date();
+    const hours = now.getHours() % 12 || 12;
+    const mints = now.getMinutes()
+    const scnds = now.getSeconds()
+    
+    hour.style.transform = `translate(-50%, -80%) rotate(${30*hours}deg)`;
+    mint.style.transform = `translate(-50%, -170px) rotate(${6*mints}deg)`;
+    scnd.style.transform = `translate(-50%, -170px) rotate(${6*scnds}deg)`;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
